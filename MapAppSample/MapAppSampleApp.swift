c19aa15e83183aa080676c614c4f5ca4a00cfd5b
Alexey Koleda <alexeykoleda@icloud.com>
2022-08-22 13:56:45 +0300

    Initial Commit

---
+++ MapAppSample/MapAppSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSampleApp.swift
+//  MapAppSample
+//
+//  Created by Alexey Koleda on 22.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
