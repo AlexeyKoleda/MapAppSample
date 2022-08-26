f20b451d47c8bccabf099552b83875bb5f33619d
Alexey Koleda <alexeykoleda@icloud.com>
2022-08-26 12:13:46 +0300

    Add data service and location model

---
+++ MapAppSample/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapAppSample
+//
+//  Created by Alexey Koleda on 26.08.2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
