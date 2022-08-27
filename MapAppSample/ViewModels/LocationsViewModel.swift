3710fe22ae829142fb30210c7f43a198ee6eb4ab
Alexey Koleda <alexeykoleda@icloud.com>
2022-08-27 14:22:44 +0300

    Add LocationsPreviewView

--- MapAppSample/ViewModels/LocationsViewModel.swift
+++ MapAppSample/ViewModels/LocationsViewModel.swift
@@ -53,4 +53,21 @@ class LocationsViewModel: ObservableObject {
             showLocationList = false
         }
     }
+    
+    func nextButtonPressed() {
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array.")
+            return
+        }
+        
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            guard let firstLocation = locations.first else { return }
+            showNewLocation(location: firstLocation)
+            return
+        }
+        
+        let nextLocation = locations[nextIndex]
+        showNewLocation(location: nextLocation)
+    }
 }
